d958d9d21723a5066abc89dccbd87cfc9107afad
Stefan <stefansandorovssb@gmail.com>
2023-08-29 14:52:22 +0200

    formulario de validación registro de usuario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registroForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    })
+  }
+
+  crearUsuario() {
+    console.log(this.registroForm.value);
+  }
 }
