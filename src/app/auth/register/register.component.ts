a7686b5c164360d479511cfb94be11eb1525f20b
Stefan <stefansandorovssb@gmail.com>
2023-10-11 16:01:10 +0200

    crear usuario con firebase

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  registroForm: FormGroup;
+export class RegisterComponent implements OnInit{
+  registroForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
     this.registroForm = this.fb.group({
       nombre: ['', Validators.required],
       correo: ['', [Validators.email, Validators.required]],
@@ -19,6 +28,15 @@ export class RegisterComponent {
   }
 
   crearUsuario() {
-    console.log(this.registroForm.value);
+
+    if (this.registroForm.invalid) {return;}
+
+    const { nombre, correo, password } = this.registroForm.value;
+    this.authService.crearUsuario(nombre, correo, password)
+    .then(credenciales => {
+      console.log(credenciales);
+      this.router.navigate(['/']);
+    })
+    .catch(console.error);
   }
 }
